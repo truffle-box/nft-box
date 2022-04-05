c402751a97554a87c87b335593b3b4549ffd8d93
leeftk <40748420+leeftk@users.noreply.github.com>
2022-04-05 11:29:16 -0400

    Update NFTCollectible.sol
    change name of contract

--- contracts/NFTCollectible.sol
+++ contracts/NFTCollectible.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract NFTCollectible is ERC721Enumerable, Ownable {
+contract NFTCollection is ERC721Enumerable, Ownable {
     using SafeMath for uint256;
     using Counters for Counters.Counter;
     
