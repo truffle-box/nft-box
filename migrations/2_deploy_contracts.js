0f67e29528aafcf567669b6792ce67fcc9f56e99
leeftk <lee.lara1219@gmail.com>
2022-04-05 11:51:44 -0400

    change contract name in migration script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var NFTCollectible = artifacts.require("./NFTCollectible.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(NFTCollectible,"ipfs://QmZbWNKJPAjxXuNFSEaksCJVd1M6DaKQViJBYPK2BdpDEP/");
+  deployer.deploy(NFTCollection,"ipfs://QmZbWNKJPAjxXuNFSEaksCJVd1M6DaKQViJBYPK2BdpDEP/");
 };
