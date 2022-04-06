57c319f2b416beddb29ffe57e57cafe6e2d7d33e
Kevin Bluer <kevin@bluer.com>
2022-04-06 11:54:26 -0500

    tidying truffle-config and bumping solidity version

--- contracts/NFTCollection.sol
+++ contracts/NFTCollection.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
