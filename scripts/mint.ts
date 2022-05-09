70090730645bd7b3750223ca2739da326c708820
emilyjlin95 <17280653+emilyJLin95@users.noreply.github.com>
2022-05-09 16:55:52 -0700

    NFTCollection Typescript Changes

--- scripts/mint.ts
+++ scripts/mint.ts
@@ -1,16 +1,17 @@
 /*
   You can use this script to quickly manually mintNFTs. To do so:
-  Run `truffle exec ./scripts/mint.js`
-  If you want to mint more than one NFT, just pass in the number
+  1. Run `tsc` to compile typescript into js
+  2. Build and deploy the NFTCollection smart contract
+  3. Run `truffle exec ./ts-out/scripts/mint.ts`
  */
 var NFTCollection = artifacts.require("./NFTCollection.sol");
 
-function getErrorMessage(error) {
+function getErrorMessage(error: unknown) {
   if (error instanceof Error) return error.message
   return String(error)
 }
 
-const main = async (cb) => {
+const main = async (cb: () => void) => {
   try {
     const args = process.argv.slice(4);
     const numNfts = args.length != 0 ? parseInt(args[0]) : 1;
