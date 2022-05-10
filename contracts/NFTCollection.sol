2f31ad587391648b9c34648602b5baea8155c677
leeftk <lee.lara1219@gmail.com>
2022-05-10 18:29:08 -0400

    add governor contract and ERC721votes to nft contract

--- contracts/NFTCollection.sol
+++ contracts/NFTCollection.sol
@@ -1,12 +1,18 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract NFTCollection is ERC721Enumerable, Ownable {
+contract NFTCollection is ERC721, ERC721Enumerable, Pausable, Ownable, ERC721Burnable, EIP712, ERC721Votes {
+    
     using SafeMath for uint256;
     using Counters for Counters.Counter;
     
@@ -19,13 +25,20 @@ contract NFTCollection is ERC721Enumerable, Ownable {
     uint public constant MAX_PER_MINT = 5;
     
     string public baseTokenURI;
-    
-    constructor(string memory baseURI, string memory name, string memory symbol) ERC721(name, symbol) {
+      constructor(string memory baseURI, string memory name, string memory symbol) ERC721(name, symbol) EIP712(name, "1"){
         setBaseURI(baseURI);
     }
+   // constructor() ERC721("MyToken", "MTK") EIP712("MyToken", "1") {}
 
+    function pause() public onlyOwner {
+        _pause();
+    }
 
-    function _baseURI() internal view virtual override returns (string memory) {
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+     function _baseURI() internal view virtual override returns (string memory) {
         return baseTokenURI;
     }
     
@@ -69,15 +82,33 @@ contract NFTCollection is ERC721Enumerable, Ownable {
         (bool success, ) = (msg.sender).call{value: balance}("");
         require(success, "Transfer failed.");
     }
-    
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        whenNotPaused
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Votes)
+    {
+        super._afterTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721) {
+        super._burn(tokenId);
+    }
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
-    // this function allows you to reserve NFTs and can only be called by owner
-    // function reserveNFTs() public onlyOwner {
-    //     uint totalMinted = _tokenIds.current();
-
-    //     require(totalMinted.add(10) < MAX_SUPPLY, "Not enough NFTs left to reserve");
-
-    //     for (uint i = 0; i < 10; i++) {
-    //         _mintSingleNFT();
-    //     }
-    // }
---
+++ contracts/NFTGovernor.sol
@@ -0,0 +1,107 @@
+ // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.6.0/governance/Governor.sol";
+import "@openzeppelin/contracts@4.6.0/governance/extensions/GovernorSettings.sol";
+import "@openzeppelin/contracts@4.6.0/governance/compatibility/GovernorCompatibilityBravo.sol";
+import "@openzeppelin/contracts@4.6.0/governance/extensions/GovernorVotes.sol";
+import "@openzeppelin/contracts@4.6.0/governance/extensions/GovernorVotesQuorumFraction.sol";
+import "@openzeppelin/contracts@4.6.0/governance/extensions/GovernorTimelockCompound.sol";
+
+contract NFTGovernor is Governor, GovernorSettings, GovernorCompatibilityBravo, GovernorVotes, GovernorVotesQuorumFraction, GovernorTimelockCompound {
+    constructor(IVotes _token, ICompoundTimelock _timelock)
+        Governor("NFTGovernor")
+        GovernorSettings(1 /* 1 block */, 45818 /* 1 week */, 0)
+        GovernorVotes(_token)
+        GovernorVotesQuorumFraction(3)
+        GovernorTimelockCompound(_timelock)
+    {}
+
+    // The following functions are overrides required by Solidity.
+
+    function votingDelay()
+        public
+        view
+        override(IGovernor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingDelay();
+    }
+
+    function votingPeriod()
+        public
+        view
+        override(IGovernor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingPeriod();
+    }
+
+    function quorum(uint256 blockNumber)
+        public
+        view
+        override(IGovernor, GovernorVotesQuorumFraction)
+        returns (uint256)
+    {
+        return super.quorum(blockNumber);
+    }
+
+    function state(uint256 proposalId)
+        public
+        view
+        override(Governor, IGovernor, GovernorTimelockCompound)
+        returns (ProposalState)
+    {
+        return super.state(proposalId);
+    }
+
+    function propose(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, string memory description)
+        public
+        override(Governor, GovernorCompatibilityBravo, IGovernor)
+        returns (uint256)
+    {
+        return super.propose(targets, values, calldatas, description);
+    }
+
+    function proposalThreshold()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.proposalThreshold();
+    }
+
+    function _execute(uint256 proposalId, address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
+        internal
+        override(Governor, GovernorTimelockCompound)
+    {
+        super._execute(proposalId, targets, values, calldatas, descriptionHash);
+    }
+
+    function _cancel(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
+        internal
+        override(Governor, GovernorTimelockCompound)
+        returns (uint256)
+    {
+        return super._cancel(targets, values, calldatas, descriptionHash);
+    }
+
+    function _executor()
+        internal
+        view
+        override(Governor, GovernorTimelockCompound)
+        returns (address)
+    {
+        return super._executor();
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(Governor, IERC165, GovernorTimelockCompound)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
